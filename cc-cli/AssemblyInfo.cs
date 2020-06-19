7799a27da51fa91cefefb10b688366461899e4a4
csannhyp <csannhyp@users.noreply.github.com>
2020-06-19 11:51:42 -0400

    Create AssemblyInfo.cs

---
+++ cc-cli/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyVersion("1.0.0.0")]
