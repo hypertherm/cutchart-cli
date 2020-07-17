2cf2c40d960429b2addd6f30f22443a3a2136f60
Harrison Saturley-Hall <h.saturley-hall@hypertherm.com>
2020-07-17 14:59:31 -0400

    Remove System.Console.WriteLine() calls and use LoggingService
    These calls were debugging output that should not be released.
    
    On an unhandled exception in the customization we should still give some output.

--- cc-cli/CcApiService.cs
+++ cc-cli/CcApiService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -233,14 +233,11 @@ namespace Hypertherm.CcCli
         {
             if(NetworkUtilities.NetworkConnectivity.IsNetworkAvailable())
             {
-                
-                Console.WriteLine($"NETWORK CONNECTED!");
                 if (ccFilename != "" && (type.ToUpper() == "XLSX" || type.ToUpper() == "DB"))
                 {
                     SetAcceptHeaderJsonContent();
 
                     var url = CcApiUtilities.BuildUrl(new[] { productName, "customs" });
-                    Console.WriteLine($"URL to create: {url}");
                     HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
                     string transformText = File.ReadAllText(xmlFilename);
                     request.Content = new StringContent(
@@ -258,10 +255,7 @@ namespace Hypertherm.CcCli
                         responseBody = await createResponse.Content.ReadAsStringAsync();
                         //response = createResponse;
                         createResponse.EnsureSuccessStatusCode();
-                        Console.WriteLine($"response to create:\n{responseBody}");
                         JObject createResponseJson = JObject.Parse(responseBody);
-
-                        Console.WriteLine($"json parse:\n{createResponseJson.ToString(Formatting.Indented)}");
 
                         if (type.ToUpper() == "XLSX")
                         {
@@ -278,7 +272,7 @@ namespace Hypertherm.CcCli
                                 createResponseJson["id"].Value<string>()
                             }
                         );
-                        Console.WriteLine($"making get custom request to : {url}");
+
                         var getCustomRequest = new HttpRequestMessage(HttpMethod.Get, url);
                         response = await _httpClient.SendAsync(getCustomRequest);
                         response.EnsureSuccessStatusCode();
@@ -298,13 +292,14 @@ namespace Hypertherm.CcCli
                     }
                     catch (HttpRequestException e)
                     {
-                        Console.WriteLine($"there is an error here: \n{e}");
                         _logger.Log(JObject.Parse(responseBody)["error"].Value<string>(), MessageType.Error);
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine($"there is an error here: \n{e}");
-                        //_logger.Log(JObject.Parse(responseBody)["error"].Value<string>(), MessageType.Error);
+                        _logger.Log(
+                            $"An unhandled error has occurredd: {e.Message}",
+                            MessageType.Error
+                        );
                     }
                 }
             }
